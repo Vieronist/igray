73c73830f749e4c1fd2ad4615eca3abbd54234f5
Andrey Dremin <sankoa230@gmail.com>
2024-11-11 00:09:42 +0300

    chore: add meta tag

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' http: https:; img-src 'self' data: http: https:; script-src 'self' 'unsafe-inline' http: https:; style-src 'self' 'unsafe-inline' http: https:;",
-          },
-        ],
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <body className="antialiased bg-[url('../public/background.jpg')]">
         <Layout>
           <Main />
