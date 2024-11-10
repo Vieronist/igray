aa0df76a4b075ae8b22f056e437ccabeac4f4335
Andrey Dremin <sankoa230@gmail.com>
2024-11-11 00:15:16 +0300

    feat: one more fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="default-src 'self' http: https:; img-src 'self' data: http: https:; script-src 'self' 'unsafe-inline' http: https:; style-src 'self' 'unsafe-inline' http: https:;"
+      />
       <body className="antialiased bg-[url('../public/background.jpg')]">
         <Layout>
           <Main />
