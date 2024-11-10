642175e27d4ea9772a927258637f131f0869c15f
Andrey Dremin <sankoa230@gmail.com>
2024-11-10 23:48:28 +0300

    refactor: next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' https:; img-src 'self' data:; script-src 'self' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
