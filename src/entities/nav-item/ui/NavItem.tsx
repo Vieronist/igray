42359515b9c319283b236d64325737984b945395
Andrey Dremin <sankoa230@gmail.com>
2024-11-14 22:27:52 +0300

    chore: changed nav item font size

--- src/entities/nav-item/ui/NavItem.tsx
+++ src/entities/nav-item/ui/NavItem.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 export const NavItem: FC<IProps> = ({ label, href }) => {
   return (
-    <li className="text-[14px] sm:text-[20px] z-10 text-gray-800">
+    <li className="text-[16px] sm:text-[20px] z-10 text-gray-800">
       <Link href={href}>{label}</Link>
     </li>
   );
