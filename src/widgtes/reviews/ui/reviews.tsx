e36ed00bee670b77dbe22d99f0f563ee1f47273a
Andrey Dremin <sankoa230@gmail.com>
2024-11-16 22:35:47 +0300

    chore: changed reviews panding

--- src/widgtes/reviews/ui/reviews.tsx
+++ src/widgtes/reviews/ui/reviews.tsx
@@ -3,7 +3,7 @@ import { AboutService } from "@/features/service";
 
 export const Reviews = () => {
   return (
-    <section className="bg-white rounded-[100px] p-[5px] mb-[60px]">
+    <section className="bg-white rounded-[100px] pt-[25px] pb-5 md:pt-[15px] px-[8px] mb-[60px]">
       <ReviewsHeader />
       <div className="flex gap-[10px] items-center flex-wrap md:flex-nowrap p-[2px] md:p-[50px]">
         <ReviewsSlider />
