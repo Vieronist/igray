b65864ed916d29f52fca447e046b1a45eb592805
Andrey Dremin <sankoa230@gmail.com>
2024-11-18 20:26:00 +0300

    one more change

--- src/widgtes/404/ui/NotFoundPanel.tsx
+++ src/widgtes/404/ui/NotFoundPanel.tsx
@@ -6,8 +6,8 @@ export const NotFoundPanel = () => {
   return (
     <div className="mb-[100px] flex justify-between">
       <div className="w-[650px]" >
-        <h3 className="text-[32px] font-extrabold mb-[23px] leading-[35.2px] mt-5">Страница, которую Вы ищете, не может быть найдена</h3>
-        <p className="text-[18px] font-normal mb-[32px]">
+        <h3 className="text-[32px] font-extrabold mb-[23px] leading-[35.2px] mt-5 text-gray-800">Страница, которую Вы ищете, не может быть найдена</h3>
+        <p className="text-[18px] font-normal mb-[32px] text-gray-800">
           Возможно, вы перешли по ссылке, в которой была допущена ошибка, или
           ресурс был удален. Попробуйте перейти на главную страницу
         </p>
