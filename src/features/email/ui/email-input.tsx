f61b131666822b232893c17bbd66878a9a1a4032
Andrey Dremin <sankoa230@gmail.com>
2024-11-19 20:06:52 +0300

    chore: one more change

--- src/features/email/ui/email-input.tsx
+++ src/features/email/ui/email-input.tsx
@@ -2,20 +2,32 @@ import { IPaymentInputs } from "@/shared";
 import { FC } from "react";
 import { UseFormRegister, FieldErrors } from "react-hook-form";
 
-
 interface IProps {
   register: UseFormRegister<IPaymentInputs>;
-  errors: FieldErrors<IPaymentInputs>
+  errors: FieldErrors<IPaymentInputs>;
 }
 
-
-export const EmailInput: FC<IProps> = ({ register}) => {
+export const EmailInput: FC<IProps> = ({ register, errors }) => {
   return (
-    <div className="border-[#DDF1EA] border rounded-[18px] py-[10px] px-[15px] mb-[15px]">
-      <label htmlFor="email" className="block text-gray-800">
-        Электронная почта
-      </label>
-      <input type="text" {...register("email", { required: false })} placeholder="name@mail.com" className="outline-none" />
-    </div>
+    <>
+      <div className="border-[#DDF1EA] border rounded-[18px] py-[10px] px-[15px] ">
+        <label htmlFor="email" className="block text-gray-800">
+          Электронная почта
+        </label>
+        <input
+          type="text"
+          {...register("email", { required: true })}
+          placeholder="name@mail.com"
+          className="outline-none"
+        />
+      </div>
+      <p className="mb-[15px]">
+        {errors.email && (
+          <span className="text-red-600 text-[12px] ml-2">
+            Электронная почта обязательна
+          </span>
+        )}
+      </p>
+    </>
   );
 };
--- src/features/steam/ui/steam-login.tsx
+++ src/features/steam/ui/steam-login.tsx
@@ -9,21 +9,24 @@ interface IProps {
 
 export const SteamLogin: FC<IProps> = ({ register, errors }) => {
   return (
-    <div className="rounded-[18px] border-[#DDF1EA] border px-[15px] py-[5px] sm:basis-[50%] mb-[5px] sm:mb-0">
-      <label htmlFor="login" className="block text-gray-800">
-        Steam логин
-      </label>
-      <input
-        type="text"
-        placeholder="Name..."
-        className="outline-none"
-        id="login"
-        {...register("login", { required: true })}
-      />
-      <div>
-
-      {errors.login && <span className="text-red-600 text-[12px]">Steam логин обязательный</span>}
+    <>
+      <div className="rounded-[18px] border-[#DDF1EA] border px-[15px] py-[5px] sm:basis-[50%] mb-[5px] sm:mb-0">
+        <label htmlFor="login" className="block text-gray-800">
+          Steam логин
+        </label>
+        <input
+          type="text"
+          placeholder="Name..."
+          className="outline-none"
+          id="login"
+          {...register("login", { required: true })}
+        />
       </div>
-    </div>
+      {errors.login && (
+        <span className="text-red-600 text-[12px] ml-2">
+          Steam логин обязательный
+        </span>
+      )}
+    </>
   );
 };
