24f746cd7ce5652b09f63a98a2b2f4bce4d48bb5
Andrey Dremin <94892594+dreminjs@users.noreply.github.com>
2024-11-22 01:23:36 +0300

    Update container.tsx

--- src/shared/ui/container.tsx
+++ src/shared/ui/container.tsx
@@ -1,6 +1,6 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-[1460px] mx-auto my-[20px] px-[15px] ">
+    <div className="max-w-[1460px] mx-auto my-[20px] px-[10px] ">
       {children}
     </div>
   )
