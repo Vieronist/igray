823588c138661947c96a126f89f89b0c637f9a88
Andrey Dremin <94892594+dreminjs@users.noreply.github.com>
2024-11-21 11:29:23 +0300

    Update social-media-item.tsx

--- src/entities/social-media/ui/social-media-item.tsx
+++ src/entities/social-media/ui/social-media-item.tsx
@@ -1,18 +1,21 @@
 import { FC } from "react";
 import { ReactSVG } from "react-svg";
+import Link from "next/link"
+
 
 interface IProps {
   svg: string;
   name: string;
+  href: string
 }
 
-export const SocialMediaItem: FC<IProps> = ({ svg, name }) => {
+export const SocialMediaItem: FC<IProps> = ({ svg, name, href }) => {
   return (
     <li className="h-[80px] w-[80px] justify-self-center p-[5px] bg-white flex items-center justify-center content-center rounded-[24px]">
-      <div className="flex flex-col items-center">
+      <Link className="flex flex-col items-center">
         <ReactSVG className="mb-[15px]" src={`/${svg}`} />
         <p className="text-center text-[12px] text-gray-800">{name}</p>
-      </div>
+      </Link>
     </li>
   );
 };
