ee7228ed3394f80915e9191cb533ede106b47242
Andrey Dremin <sankoa230@gmail.com>
2024-11-21 20:36:45 +0300

    one more change

--- pages/error/index.tsx
+++ pages/error/index.tsx
@@ -4,14 +4,45 @@ import { Footer } from "@/widgtes/footer";
 import { Header } from "@/widgtes/header";
 import { Reviews } from "@/widgtes/reviews";
 
+interface IArticle {
+  content: string;
+  avatar: string;
+}
+
 export default function Index() {
-    return (
-        <div>
-            <Header />
-            <ErrorPanel />
-            <ArticlesList className="hidden xl:flex justify-center mb-[120px] mx-auto" />
-            <Reviews className="hidden xl:block" />
-            <Footer />
-        </div>
-    )
+  const articles: IArticle[] = [
+    {
+      content: "Как пополнить свой Steam бесплатно?",
+      avatar: "gabe.png",
+    },
+    {
+      content: "Что было на презентации Xbox 2024",
+      avatar: "someone.png",
+    },
+    {
+      avatar: "dracon.png",
+      content: "Магический выживач Nightingale",
+    },
+    {
+      avatar: "baba-v-shlyape.png",
+      content: "Foamstars выйдет 6 февраля",
+    },
+    {
+      avatar: "overwatch-baba.png",
+      content: "Чарт продаж Steam захватили фритуплей",
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <ErrorPanel />
+      <ArticlesList
+        articles={articles}
+        className="hidden xl:flex justify-center mb-[120px] mx-auto"
+      />
+      <Reviews className="hidden xl:block" />
+      <Footer />
+    </div>
+  );
 }
