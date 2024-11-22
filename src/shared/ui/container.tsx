ac912ae57f193026c7f38db39ce73000fa359923
Andrey Dremin <sankoa230@gmail.com>
2024-11-22 21:38:56 +0300

    Разрешены конфликты слияния

--- src/shared/ui/container.tsx
+++ src/shared/ui/container.tsx
@@ -1,6 +1,10 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
+<<<<<<< Updated upstream
     <div className="max-w-[1460px] mx-auto my-[20px] px-[8px] ">
+=======
+    <div className="max-w-[1470px] mx-auto my-[20px] px-[15px] ">
+>>>>>>> Stashed changes
       {children}
     </div>
   )
